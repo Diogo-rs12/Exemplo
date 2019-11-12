d9cb86c4c3396333a6642d358879762cf041ab60
unknown <rodriguesdiogo454@gmail.com>
2019-11-12 16:12:37 -0200

    Primeiro envio do projeto

---
+++ Principal.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Principal {
+  public static void main(String[] args) {
+    Scanner dr = new Scanner(System.in);
+    int n = dr.nextInt();
+    int r = fib(n);
+    System.out.println(r);
+  }
+static int fib(int m) {
+  if(m == 0){return 0;}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+  if(m == 1){return 1;}
+  
+  return fib(m-1)+fib(m-2);
+ }
+}
---
+++ Principall.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+class Principal {
+  public static void main(String[] args) {
+    Scanner  klawiatura= new Scanner(System.in);
+
+    
